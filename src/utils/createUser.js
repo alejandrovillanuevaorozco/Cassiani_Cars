befa47a5ee9c6b6c9e5a4bebdd77de027ca14f11
Andres Barrios <anmabadi@outlook.com>
2026-06-02 15:10:30 -0500

    forgotten commit

--- src/utils/createUser.js
+++ src/utils/createUser.js
@@ -1,8 +1,13 @@
 import {endPointUsers} from "./validatiion"
 
-export async function signUpUser(){
+export async function signUpUser(user){
     try {
-        const response = await fetch (endPointUsers)
+        const response = await fetch (endPointUsers, {
+            method: 'POST',
+            headers:{ 'Content-Type':'application/json'},
+            body: JSON.stringify(user)
+        });
+        if(!response.ok) throw new Error('Error!')
     } catch (error) {
         
     }
